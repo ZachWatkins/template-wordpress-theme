c05f420cd2cd31c0b6877c7a93fbfb4cbb428f2a
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2022-10-26 22:07:35 -0500

    Apply changes based on code standard

--- functions.php
+++ functions.php
@@ -2,9 +2,10 @@
 /**
  * Template Genesis Theme functions and definitions.
  *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
  * @package template-genesis-theme
+ * @author  Zachary K. Watkins <zwatkins.it@gmail.com>
+ * @license GPL-2.0+
+ * @link    https://developer.wordpress.org/themes/basics/theme-functions/
  */
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -21,15 +22,16 @@ add_action( 'wp_enqueue_scripts', 'genesis_parent_theme_enqueue_styles' );
  * @return void
  */
 function genesis_parent_theme_enqueue_styles(): void {
-	wp_enqueue_style(
-		'genesis-style',
-		get_template_directory_uri() . '/style.css',
+    wp_enqueue_style(
+        'genesis-style',
+        get_template_directory_uri() . '/style.css',
         false,
         filemtime( get_template_directory() . '/style.css' )
-	);
-	wp_enqueue_style(
-		'template-genesis-theme-style',
-		get_stylesheet_directory_uri() . '/style.css',
-		array( 'genesis-style' ),
-	);
+    );
+    wp_enqueue_style(
+        'template-genesis-theme-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'genesis-style' ),
+        filemtime( get_stylesheet_directory_uri() . '/style.css' )
+    );
 }
